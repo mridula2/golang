e0f5dec01f687d0115a1d3c4c4a7e48ffaa6c42d
mridula krishnamurthy <mridu94@gmail.com>
2022-03-30 14:32:21 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
-import "log"
-import "net/http"
-
-type Server struct{}
-
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-   w.WriteHeader(http.StatusOK)
-   w.Header().Set("Content-Type", "application/json")
-   w.Write([]byte(`{"message": "hello world"}`))
-}
+import (
+    "fmt"
+    "html"
+    "log"
+    "net/http"
+)
 
 func main() {
-   s := &Server{}
-   http.Handle("/", s)
-   log.Fatal(http.ListenAndServe(":8080", nil))
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
+
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hi")
+    })
+
+    log.Fatal(http.ListenAndServe(":8081", nil))
+
 }
